47d1929eeb733d736da331d5e17a321d8fe750d2
wompscode <phoebe@womp.gay>
2026-04-30 15:49:04 +1000

    API15 + change to default settings

--- NowPlaying/Configuration.cs
+++ NowPlaying/Configuration.cs
@@ -8,7 +8,7 @@ public class Configuration : IPluginConfiguration
 {
     public int Version { get; set; } = 0;
     public bool ShowInStatusBar { get; set; } = true;
-    public bool HideOnPause { get; set; } = true;
+    public bool HideOnPause { get; set; } = false;
     public bool Truncate { get; set; } = true;
     public int MaxSongChars { get; set; } = 24;
     public int MaxArtistChars { get; set; } = 18;
